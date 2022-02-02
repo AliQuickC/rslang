61e3821ae4c5a1dc1253997fde7e456f48f5511a
Aleksandr <aleksandera9@gmail.com>
2022-02-02 16:20:10 +0300

    feat: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+
+console.log('IS PROD', isProd);
+console.log('IS DEV', isDev);
+
+const devServer = (isDev) => !isDev ? {} : {
+	devServer: {
+		open: true,
+		hot: true,
+		liveReload: true,
+		port: 3000,
+		static: ['./src', './public'],
+	},
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+	mode: isProd === 'production' ? 'production' : 'development',
+  devtool: isDev ? 'inline-source-map' : false,
+  entry: './index.ts',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+
+	resolve: {
+    extensions: ['.ts', '.js'],
+	},
+
+	module: {
+    rules: [
+			{
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+			{
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      }
+    ]
+  },
+
+	plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+	
+	...devServer(isDev)
+};
