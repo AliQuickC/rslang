5aae3800c6f62c8d12cc715a771c64091beb7625
Aleksandr <aleksandera9@gmail.com>
2022-02-05 23:50:40 +0300

    feat: add api for words

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -38,3 +38,60 @@ export const RenderPage = {
   statistics: renderStatistics,
   developmentTeam: renderTeam,
 };
+
+export interface Word {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  textExampleTranslate: string;
+  textMeaningTranslate: string;
+  wordTranslate: string;
+}
+
+export interface UserWord {
+  difficulty: string;
+  optional: object;
+}
+
+export interface aggregatedUserWord extends Word {
+  userWord: UserWord | undefined;
+}
+
+export type aggregatedUserWords = [
+  {
+    paginatedResults: aggregatedUserWord[] | [];
+    totalCount: [{ count: number }] | [];
+  }
+];
+
+export interface Statistic {
+  learnedWords: number;
+  optional: object;
+}
+
+export interface Setting {
+  wordsPerDay: number;
+  optional: object;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface Auth {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name: string;
+}
