185f1f83e5319b98669789eee048f8d92b048203
Aleksandr <aleksandera9@gmail.com>
2022-02-22 00:54:40 +0300

    feat: deploy rslang

--- src/components/utilites/getHtmlFromString.ts
+++
@@ -1,5 +0,0 @@
-export default function getHtmlFromString(htmlFromString: string) {
-  const template = document.createElement('template');
-  template.innerHTML = htmlFromString;
-  return template.content as DocumentFragment;
-}
